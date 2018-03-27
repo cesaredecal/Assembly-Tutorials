57f6a989c5efb267b192082538cc0901d2cc4074
Cesare de Cal <contact@cesare.io>
2018-03-27 16:39:13 +0200

    Multiplication

---
+++ MultiplyingIntegersMul.asm
@@ -0,0 +1,10 @@
+.data
+
+.text
+	addi $s0, $zero, 10 # immediate is a constant, add 10 plus 0 and store the sum in the $s0 register
+	addi $s1, $zero, 4
+	
+	mul $t0, $s0, $s1 # it takes 3 registers, take $s0 and $s1 and store product in $t0
+	li $v0, 1
+	add $a0, $zero, $t0 # store product in $a0
+	syscall
