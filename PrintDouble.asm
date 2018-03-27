eef41506bc05d541e49c4073e672c812c41c200d
Cesare de Cal <contact@cesare.io>
2018-03-27 16:23:57 +0200

    Printing doubles and adding integers

--- PrintDouble.asm
+++ PrintDouble.asm
@@ -2,6 +2,10 @@
 	myDouble: .double 7.202
 	zeroDouble: .double 0.0
 .text
+	# it takes two registers
+	# it always takes a pair
+	# always use even register numbers
+	# it's a special case, it uses 64 bits instead of 32
 	ldc1 $f2, myDouble
 	ldc1 $f0, zeroDouble
 	li $v0, 3 # 3 is the number for doubles
