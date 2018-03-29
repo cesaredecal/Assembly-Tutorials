4954f9f3d6ff3632b0d8b8d6fc566d644f8820b9
Cesare de Cal <contact@cesare.io>
2018-03-29 10:46:36 +0200

    Nested procedures

--- NestedProcedures.asm
+++ NestedProcedures.asm
@@ -6,10 +6,7 @@
 		addi $s0, $zero, 10
 		
 		jal increaseMyRegister
-		
-		li $v0, 1
-		move $a0, $s0
-		syscall
+		jal printTheValue
 	
 	# This line is going to signal end of program
 	li $v0, 10
@@ -19,26 +16,33 @@
 	# it's different for s registers
 	# we don't want procedures to modify s registers so we save the old value to a stack
 	increaseMyRegister:
-		# in order to use $s registers in functions, you are supposed to save the old value
-		# to do so, allocate enough space in the stack
-		addi $sp, $sp, -4 # allocates 4 bytes, stack goes does, minus because we're allocating space
+		# every value is 4 bytes
+		addi $sp, $sp, -8 # allocates 8 bytes, stack goes does, minus because we're allocating space
+
 		sw $s0, 0($sp) # save value to first location in the stack pointer
-		
+		sw $ra, 4($sp) # the address will be stored  
 		
 		# after that we can do whatever we want in the register
 		addi $s0, $s0, 30
 		
-		# print new value in function.
-		li $v0, 1
-		move $a0, $s0
-		syscall
+		# nested procedure
+		jal printTheValue
 		
 		# restoring value of the stack by adding 4
 		lw $s0, 0($sp)
+		lw $ra, 4($sp)
+		
 		addi $sp, $sp, 4
 		
 		li $v0, 4
 		la $a0, newLine
 		syscall
 		
+		jr $ra
+		
+	printTheValue:
+		# print new value in function.
+		li $v0, 1
+		move $a0, $s0
+		syscall
 		jr $ra
